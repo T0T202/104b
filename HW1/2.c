7f4a5df745962cad05839d9442d8e1c0bc9fcaee
Thanh To <thanhyto.per@gmail.com>
2022-01-12 09:11:31 -0800

    finished 5

--- HW1/2.c
+++ HW1/2.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
-
 int main()
 {   
     //Declare all the variables
     float a,b,c,d,e,f,x,y;
 
     //Input a, b, c, d, e, f
-    printf("Enter the coefficients for the first line: \n" );
+    printf("Enter the coefficients for the first line: " );
     scanf("%f %f %f", &a, &b, &c);
-    
-
     printf("Enter the coefficients for the second line: ");
-        scanf("%f %f %f", &d, &e, &f);
-
-
+    scanf("%f %f %f", &d, &e, &f);
 
     //Find x and y
     x = (b*f - e*c)/(b*d - a*e);
     y = (d*c - a*f)/(b*d - a*e);
 
-    // 3 cases
+    // 3 cases, overlap, parallel and intersect
     if((a/b) == (d/e)){
         if((a == d) && (b == e) && (c == f)){
             printf("The two lines overlap. Infinite solutions");
@@ -32,6 +27,5 @@ int main()
         printf("The two lines intersect. Unique solution\n");
         printf("The point of intersect: %f, %f", x, y);
     }
-
     return 0;
 }
--- HW1/5.py
+++ HW1/5.py
@@ -6,6 +6,8 @@ iterationLength = 0
 startingNumber = 0
 #Largest value
 largestNumber = 0
+#Largest value starting nummber
+largestStartingNumber = 0
 #Run loops of iteration from 0 to 1000
 for i in range(0, 1001):
     length = 1
@@ -19,9 +21,13 @@ for i in range(0, 1001):
         else:
             n = n // 2
         length = length + 1
+    if(n>largestNumber):
+        largestNumber = n
+        largestStartingNumber = i
     #Compare the current length of the loop with the previous longest iteration, if exceeds, assigned length to iterationLength and make starting number equal to i
     if (length > iterationLength):
         iterationLength = length
         startingNumber = i
 
 print("The starting number is: ",startingNumber," and the number of iterations is: ", iterationLength)
+print("The largest value is ", largestNumber, " and the starting number for that is ", largestStartingNumber)
