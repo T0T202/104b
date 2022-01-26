9ba7ebe970f2bfee1433074712b93b617eba7e27
Thanh To <thanhyto.per@gmail.com>
2022-01-25 18:55:26 -0800

    add more to problem 2, finish problem 3

--- HW3/2.py
+++ HW3/2.py
@@ -1,7 +1,7 @@
 #Assignment 3 Problem 2
 
 import numpy as np
-
+import math
 #Define dimension of a vector
 d = int(input("Please enter the dimensions of the vector: "))
 #Define the 2 vector array
@@ -21,9 +21,18 @@ for i in range (0, d):
     v2.append(v2i)
 
 #initial sum 
-sum = 0
+dot = 0 #use to calculate the dot product
+sum1 = 0 #use to calculate the length of vector 1
+sum2 = 0 #use to calculate the length of vector 2
 #Calculate the dot product between 2 vector
 for i in range(0, d):
-    sum = sum + v1[i] * v2[i]
+    dot = dot + v1[i] * v2[i]
+    #The length of each vector
+    sum1 = sum1 + v1[i] ** 2
+    sum2 =  sum2 + v2[i] ** 2
+v1_length = np.sqrt(sum1)
+v2_length = np.sqrt(sum2)
 
-print("The dot product of the 2 vectors is: ", sum)
+print("The dot product of the 2 vectors is: ", dot)
+print("The cosine value of the angle between 2 vectors is: ", dot/(v1_length * v2_length))
+print("The angle between the 2 vectors is: ", math.acos(dot/(v1_length * v2_length)))
